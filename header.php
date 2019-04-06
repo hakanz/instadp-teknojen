50e0bdc785a4c2014a80d1b9bdcc89f8e58585f0
Hakan <hakanz@users.noreply.github.com>
2019-04-07 02:55:55 +0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Instagram Display Picture Downloader - Full Size</title>
+		<meta name="title" content="Instagram Display Picture Downloader - Full Size">
+		<meta name="description" content="With this tool you can see full size Instagram profile pictures only with two simple steps.">
+		<!-- FB -->
+		<meta property="og:type" content="website">
+		<meta property="og:url" content="https://apps.teknojen.net/ig">
+		<meta property="og:title" content="Instagram Display Picture Downloader - Full Size">
+		<meta property="og:description" content="With this tool you can see full size Instagram profile pictures only with two simple steps.">
+		<!--<meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"> -->
+
+		<!-- Twitter 
+		<meta property="twitter:card" content="summary_large_image">
+		<meta property="twitter:url" content="https://metatags.io/">
+		<meta property="twitter:title" content="Meta Tags — Preview, Edit and Generate">
+		<meta property="twitter:description" content="With this tool you can see full size Instagram profile pictures only with two simple steps.">-->
+		<!--<meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"> -->
+
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" crossorigin="anonymous">
+		<script src="js/jquery.js" crossorigin="anonymous"></script>
+	</head>
